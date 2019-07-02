d42d1d5f4d4d316f93303b10c9615db76173107a
Mydatalinx <yoann.maingon@mydatalinx.com>
2019-07-02 10:03:06 +0200

    update gulp and packages and remove actual client

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,9 @@ var gulp = require('gulp');
 gulp.task('default', function () {
     console.log("default task")
 });
-gulp.task('copy-apidoc-fix', function () {
+gulp.task('copy-apidoc-fix', function (done) {
     gulp.src('./custo/send_sample_request.js')
         .pipe(gulp.dest('./node_modules/apidoc/template/utils/'));
+        
+  done();
 });
